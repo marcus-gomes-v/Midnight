c7803fad2f34b6ed40f654010c4089f70af7a0b2
Vinicius Gomes <viniciusllgomes@gmail.com>
2019-11-25 14:31:47 -0300

    Start GIT for Midnight

---
+++ index.js
@@ -0,0 +1,31 @@
+/*
+ * PRIMARY FILE FOR THE API
+ *
+ */
+
+ // Load Dependencies
+
+ const server = require(`${__dirname}/lib/server`)
+ const workers = require(`${__dirname}/lib/workers`)
+
+
+ // Declare the App
+const app = {};
+
+
+// Init function
+app.init = () => {
+    // Start the server
+    server.init()
+
+    // Start the workers
+      workers.init() 
+}
+
+ 
+// Execute
+app.init()
+
+// Export the app
+module.exports = app;
+
