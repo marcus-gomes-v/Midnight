204eea9d76e241d8f1a16f52209909d1c8cc3959
Vinicius Gomes <viniciusllgomes@gmail.com>
2019-12-05 02:59:46 -0300

    Implementation of CLI

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 
  const server = require(`${__dirname}/lib/server`)
  const workers = require(`${__dirname}/lib/workers`)
+ const cli = require(`${__dirname}/lib/cli`)
 
 
  // Declare the App
@@ -20,6 +21,11 @@ app.init = () => {
 
   // Start the workers
   workers.init() 
+
+  // Start the CLI, but make sure it starts last
+  setTimeout(() => {
+    cli.init();
+  }, 50);
 }
 
  
